0f51c8f020c753f284b91b00ca0a2fd5b6053f04
Gage Peterson <gpeterson@instructure.com>
2015-07-08 10:11:18 -0600

    added some stuff for heroku

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,13 +4,13 @@ use Mix.Config
 # you likely want to automate and keep it away from
 # your version control system.
 config :vanpool, Vanpool.Endpoint,
+  debug_errors: true,
   secret_key_base: System.get_env("SECRET_KEY_BASE") 
-  # secret_key_base: 
 
 # Configure your database
 config :vanpool, Vanpool.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: System.get_env("DATABASE_USERNAME"),
   password: System.get_env("DATABASE_PASSWORD"),
-  database: "vanpool_prod",
+  database: System.get_env("DATABASE_NAME"),
   size: 20 # The amount of database connections in the pool
